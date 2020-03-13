6b09e5eb2359038b6761f62de1dcd9b927795a6b
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-03-13 12:01:22 -0600

    completed mvp, maybe stretch

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,37 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle():
+    def __init__(self):
+        self.vehicle = "Its a vehicle"
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        self.flight_vehivle = "It can fly!"
+        super().__init__()
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        self.starship = "Its a space ship"
+        super().__init__()
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        self.ground_vehicle = "Its a vehicle for the ground"
+        super().__init__()
+
+class Car(GroundVehicle):
+    def __init__(self):
+        self.ground_vehicle = "Its a car"
+        super().__init__()
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        self.ground_vehicle = "Its a motorcycle"
+        super().__init__()
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        self.ground_vehicle = "Its an airplane"
+        super().__init__()
